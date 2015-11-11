03cc787f63f4cbcf7fe0f102ff1c5a9ba29d09d6
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2015-11-11 13:49:42 +0100

    custom: shrink overlay
    Shrink the overlay, as s6-linux-init-skeleton now provides all the
    needed services and scripts.
    
    The only custom service left is sshd.

--- custom/board/common/post-build.sh
+++ custom/board/common/post-build.sh
@@ -9,4 +9,5 @@ mkdir -p ${TARGET_DIR}/etc/dropbear
 echo "Compiling s6-rc service database"
 rm -rf ${TARGET_DIR}/etc/s6-rc/compiled
 mkdir -p ${TARGET_DIR}/etc/s6-rc
-${HOST_DIR}/usr/bin/s6-rc-compile -v 3 ${TARGET_DIR}/etc/s6-rc/compiled ${BR2_EXTERNAL}/board/common/s6-rc/source
+${HOST_DIR}/usr/bin/s6-rc-compile -v 3 ${TARGET_DIR}/etc/s6-rc/compiled \
+           ${TARGET_DIR}/etc/s6-rc/source
