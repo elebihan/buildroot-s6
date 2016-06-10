6d83dc35ff4d5806fb8aceec49555c3588cfb22e
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-06-10 21:31:01 +0200

    s6-linux-init-services: support dropbear
    Add s6-rc service directory for dropbear.

--- buildroot/package/s6-linux-init-services/s6-linux-init-services.mk
+++ buildroot/package/s6-linux-init-services/s6-linux-init-services.mk
@@ -10,6 +10,17 @@ S6_LINUX_INIT_SERVICES_DEPENDENCIES = s6-linux-init-skeleton
 
 S6_LINUX_INIT_SERVICES_STORE = package/s6-linux-init-services/files/etc/s6-rc/source
 
+ifeq ($(BR2_PACKAGE_DROPBEAR),y)
+define S6_LINUX_INIT_SERVICES_DROPBEAR
+	cp -a $(S6_LINUX_INIT_SERVICES_STORE)/dropbear \
+		$(TARGET_DIR)/etc/s6-rc/source
+	cp -a $(S6_LINUX_INIT_SERVICES_STORE)/dropbear-log \
+		$(TARGET_DIR)/etc/s6-rc/source
+	echo dropbear >> $(TARGET_DIR)/etc/s6-rc/source/bundle-lan/contents
+endef
+S6_LINUX_INIT_SERVICES_ITEMS += S6_LINUX_INIT_SERVICES_DROPBEAR
+endif
+
 define S6_LINUX_INIT_SERVICES_INSTALL_INIT_S6_INIT
 	$(foreach item,$(S6_LINUX_INIT_SERVICES_ITEMS),$(call $(item))$(sep))
 endef
