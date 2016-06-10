9a25e45c006913d4f841d1fe2561f782b13daed7
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-06-10 21:52:08 +0200

    s6-linux-init-services: support rngd
    Add a s6-rc service directory for rngd.

--- buildroot/package/s6-linux-init-services/s6-linux-init-services.mk
+++ buildroot/package/s6-linux-init-services/s6-linux-init-services.mk
@@ -21,6 +21,17 @@ endef
 S6_LINUX_INIT_SERVICES_ITEMS += S6_LINUX_INIT_SERVICES_DROPBEAR
 endif
 
+ifeq ($(BR2_PACKAGE_RNG_TOOLS),y)
+define S6_LINUX_INIT_SERVICES_RNGD
+	cp -a $(S6_LINUX_INIT_SERVICES_STORE)/rngd \
+		$(TARGET_DIR)/etc/s6-rc/source
+	cp -a $(S6_LINUX_INIT_SERVICES_STORE)/rngd-log \
+		$(TARGET_DIR)/etc/s6-rc/source
+	echo rngd >> $(TARGET_DIR)/etc/s6-rc/source/bundle-local/contents
+endef
+S6_LINUX_INIT_SERVICES_ITEMS += S6_LINUX_INIT_SERVICES_RNGD
+endif
+
 define S6_LINUX_INIT_SERVICES_INSTALL_INIT_S6_INIT
 	$(foreach item,$(S6_LINUX_INIT_SERVICES_ITEMS),$(call $(item))$(sep))
 endef
