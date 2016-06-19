be5ec230a02abc6d67b28494fbfa8e45741fd972
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-06-19 23:08:55 +0200

    custom/common: refactor post-build.sh
    Refactor post-build.sh by introducing a function to rebuild the s6-rc
    database.
    
    This will make the script cleaner when adding new steps.

--- custom/board/common/post-build.sh
+++ custom/board/common/post-build.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-echo "Compiling s6-rc service database"
-rm -rf ${TARGET_DIR}/etc/s6-rc/compiled
-mkdir -p ${TARGET_DIR}/etc/s6-rc
-${HOST_DIR}/usr/bin/s6-rc-compile -v 3 ${TARGET_DIR}/etc/s6-rc/compiled \
-           ${TARGET_DIR}/etc/s6-rc/source
+compile_s6_rc_db()
+{
+    echo "Compiling s6-rc service database"
+    rm -rf ${TARGET_DIR}/etc/s6-rc/compiled
+    mkdir -p ${TARGET_DIR}/etc/s6-rc
+    ${HOST_DIR}/usr/bin/s6-rc-compile -v 3 ${TARGET_DIR}/etc/s6-rc/compiled \
+               ${TARGET_DIR}/etc/s6-rc/source
+}
+
+compile_s6_rc_db
