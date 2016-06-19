52c7e3206bd250d0456dcd592f61f4618cecb0e7
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-06-19 22:09:39 +0200

    custom/odroidc2: add getty-tty1 to services-local
    As bundle-local has been replaced by services-local, update
    post-build.sh accordingly to enable getty-tty1.

--- custom/board/hardkernel/odroidc2/post-build.sh
+++ custom/board/hardkernel/odroidc2/post-build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-LOCAL_SERVICES="${TARGET_DIR}/etc/s6-rc/source/bundle-local/contents"
+LOCAL_SERVICES="${TARGET_DIR}/etc/s6-rc/source/services-local/contents"
 
 echo "Enabling s6-rc services"
 if ! grep -q getty-tty1 "${LOCAL_SERVICES}"; then
