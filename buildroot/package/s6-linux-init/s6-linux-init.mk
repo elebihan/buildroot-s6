e27a573823e9134925dcb26f96f564259d5b2a7a
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-08-09 18:37:34 +0200

    s6-linux-init: use tarball instead of git repo

--- buildroot/package/s6-linux-init/s6-linux-init.mk
+++ buildroot/package/s6-linux-init/s6-linux-init.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-S6_LINUX_INIT_VERSION = v0.1.0.0
-S6_LINUX_INIT_SITE = git://git.skarnet.org/s6-linux-init.git
+S6_LINUX_INIT_VERSION = 0.1.0.0
+S6_LINUX_INIT_SITE = http://skarnet.org/software/s6-linux-init
 S6_LINUX_INIT_LICENSE = ISC
 S6_LINUX_INIT_LICENSE_FILES = COPYING
 S6_LINUX_INIT_DEPENDENCIES = s6 s6-linux-utils s6-portable-utils
