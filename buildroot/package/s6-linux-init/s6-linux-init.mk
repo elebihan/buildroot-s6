d55226d740de2a13a8ac41d52d42ffd9c4361ad9
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-08-10 18:40:58 +0200

    s6-linux-init: use $(PKG)_CONF_OPTS

--- buildroot/package/s6-linux-init/s6-linux-init.mk
+++ buildroot/package/s6-linux-init/s6-linux-init.mk
@@ -10,7 +10,7 @@ S6_LINUX_INIT_LICENSE = ISC
 S6_LINUX_INIT_LICENSE_FILES = COPYING
 S6_LINUX_INIT_DEPENDENCIES = s6 s6-linux-utils s6-portable-utils
 
-S6_LINUX_INIT_CONFIGURE_OPTS = \
+S6_LINUX_INIT_CONF_OPTS = \
 	--prefix=/usr \
 	--with-sysdeps=$(STAGING_DIR)/usr/lib/skalibs/sysdeps \
 	--with-include=$(STAGING_DIR)/usr/include \
@@ -20,13 +20,13 @@ S6_LINUX_INIT_CONFIGURE_OPTS = \
 	--with-lib=$(STAGING_DIR)/usr/lib/skalibs
 
 ifeq ($(BR2_STATIC_LIBS),y)
-S6_LINUX_INIT_CONFIGURE_OPTS +=  --enable-static --disable-shared
+S6_LINUX_INIT_CONF_OPTS +=  --enable-static --disable-shared
 else
-S6_LINUX_INIT_CONFIGURE_OPTS +=  --disable-static --enable-shared --disable-allstatic
+S6_LINUX_INIT_CONF_OPTS +=  --disable-static --enable-shared --disable-allstatic
 endif
 
 define S6_LINUX_INIT_CONFIGURE_CMDS
-	(cd $(@D); $(TARGET_CONFIGURE_OPTS) ./configure $(S6_LINUX_INIT_CONFIGURE_OPTS))
+	(cd $(@D); $(TARGET_CONFIGURE_OPTS) ./configure $(S6_LINUX_INIT_CONF_OPTS))
 endef
 
 define S6_LINUX_INIT_BUILD_CMDS
